aff40a8b3b44a8c4c814059e45d67e9289d86a91
Dicki <bukandicki@gmail.com>
2022-09-12 14:47:28 +0700

    feat: Add tailwind css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,27 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-prettier"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        "no-trailing-spaces": "error",
+        "eol-last": ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "no-unused-vars": "off",
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto"
+            }
+        ]
+    }
 };
